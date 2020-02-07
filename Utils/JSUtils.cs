975256a4c447b61b5a6092dd732448c0866583a7
Alex Copindean <alexacopindean@yahoo.com>
2020-02-07 15:16:46 +0200

    Add files

---
+++ Utils/JSUtils.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+
+namespace Autofocus_Demo.Utils
+{
+
+    public class JSUtils
+    {
+
+        public static void RequestFocus(IJSRuntime runtime, string selector)
+        {
+            runtime.InvokeVoidAsync("requestFocus", selector);
+        }
+    }
+}
---
+++ wwwroot/js/autofocus.js
@@ -0,0 +1,11 @@
+﻿window.addEventListener("load", () => {
+    const observer = new MutationObserver(() => {
+        const elementToFocus = document.querySelector(".autofocus");
+        elementToFocus && elementToFocus.focus();
+    })
+
+    observer.observe(document, { childList: true, subtree: true });
+});
+
+window.requestFocus = selector => 
+    document.querySelector(selector).focus();
